ea928fabe600b42868600fee7d3c2fd222afc98f
Eliot Kid <eliotfonse99@gmail.com>
2026-06-16 16:14:11 -0300

    first commit

---
+++ components/button/button.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss"
+
+const Button = () => {
+  return ( 
+    <div className={styles.button }></div>
+   );
+}
+ 
+export default Button;
---
+++ components/card/card.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss"
+
+const Card = () => {
+  return ( 
+    <div className={styles.button }></div>
+   );
+}
+ 
+export default Card;
